71e7cf33c1246a60ba58b118e6053c4a1d17af0b
laoyang <ycr1714336458@163.com>
2026-01-15 17:36:06 +0800

    feature: 补充下

--- src/content/_meta.js
+++ src/content/_meta.js
@@ -2,5 +2,6 @@ export default {
   'index': '就是占个位',
   'next-learn': 'next相关',
   'spreadsheet': '数据库',
-  'vue3': 'vue3读码'
+  'vue3': 'vue3读码',
+  'py': 'python',
 }
--- src/content/py/_meta.js
+++ src/content/py/_meta.js
@@ -6,4 +6,8 @@ export default {
   'day_5': 'Day 5',
   'day_7': 'Day 7', 
   'day_8': 'Day 8',
+  'day_9': 'Day 9',
+  'day_10': 'Day 10',
+  'day_12': 'Day 12',
+  'day_13': 'Day 13',
 }
