ff79edea09a62e01b0590aba6dc2feda475aa261
laoyang <ycr1714336458@163.com>
2026-03-30 17:12:57 +0800

    feature: 阶段性文档总结

--- src/content/py/django/DRF/_meta.js
+++ src/content/py/django/DRF/_meta.js
@@ -1,6 +1,11 @@
 export default {
     'serializer_base_1': 'serializer基础1',
-    'view_base_1': '视图基础1',
-    'view_base_2': '视图基础2',
     'serializer_base_2': 'serializer基础2',
+    'serializer_base_3': 'serializer阶段总结',
+    'APIView_1': 'APIView_1',
+    'APIView_2': 'APIView_2',
+    'generic_views': '通用视图',
+    'ViewSet_1': 'ViewSet_1',
+    'Viewset_2': 'ViewSet_2',
+    'pagination': '分页',
 }
